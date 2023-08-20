6c7c88aae1cfa61f7940967145aa01eaa8f20d80
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-08-20 17:26:06 +0300

    return true or false in input values

--- two_numbers_positive.py
+++ two_numbers_positive.py
@@ -1,1 +1,12 @@
-#code goes here
+def positive_numbers(a, b, c):
+    count = 0
+    for number in [a, b, c]:
+        if number > 0:
+            count += 1
+    return count == 2
+
+a = int(input("First Number is: "))
+b = int(input("Second Number is: "))
+c = int(input("Third Number is: "))
+
+print(positive_numbers(a, b, c))
