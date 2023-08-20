a1acd3cb5e88173e71babe3a0c67d6d9ee0af671
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-08-20 17:58:38 +0300

    created time converter

--- time_converter.py
+++ time_converter.py
@@ -1,1 +1,12 @@
-#code goes here
+def time_converter(hour, minute, period):
+    if hour == 12 and period == "am":
+        hour = 0
+    elif hour !=12 and period =="pm":
+        hour += 12
+    return(f"{hour:02d} : {minute:02d}")
+
+hour = int(input("Hour: "))
+minute = int(input("Minute: "))
+period = input("Period: ")
+
+print(time_converter(hour, minute, period))
